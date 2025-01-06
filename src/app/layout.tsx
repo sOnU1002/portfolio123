672789ea25323a763098ac7b6a0a74b0414234c7
Saket Nigam <113360927+sOnU1002@users.noreply.github.com>
2025-01-06 14:57:39 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "Ted Thoughts",
+  title: "Saket Thoughts",
   description: "My personal site to showcase my developer work and opinions.",
 };
 
